f41621ece943880ebf4c364d2f0c3b8cb108f45d
It423 <it42@hotmail.co.uk>
2014-11-13 17:34:00 +0000

    Add procedures to handle mouse inputs

---
+++ src/UMouseInputs.pas
@@ -0,0 +1,36 @@
+unit UMouseInputs;
+
+interface
+
+uses 
+  W3System, W3Components;
+
+procedure MouseDownHandler(o : TObject; b : TMouseButton; t : TShiftState; x, y : integer);
+procedure MouseUpHandler(o : TObject; b : TMouseButton; t : TShiftState; x, y : integer);
+procedure MouseMoveHandler(o : TObject; ss : TShiftState; x, y : integer);
+
+var
+  MouseDown : boolean;
+  MouseDownX, MouseDownY, CurrentMouseX, CurrentMouseY : integer;
+
+implementation
+
+procedure MouseDownHandler(o : TObject; b : TMouseButton; t : TShiftState; x, y : integer)
+begin
+  MouseDown := true;
+  MouseDownX := x;
+  MouseDownY := y;
+end;
+
+procedure MouseUpHandler(o : TObject; b : TMouseButton; t : TShiftState; x, y : integer)
+begin
+  MouseDown := false;
+end;
+
+procedure MouseMoveHandler(o : TObject; ss : TShiftState; x, y : integer)
+begin
+ CurrentMouseX := x;
+ CurrentMouseY := y;
+end;
+
+end.
