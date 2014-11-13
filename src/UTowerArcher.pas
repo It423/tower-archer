67076416f95d1f524d24f6fad9960e1b8ca73514
It423 <it42@hotmail.co.uk>
2014-11-13 17:33:17 +0000

    Subscribe to mouse events

--- src/UTowerArcher.pas
+++ src/UTowerArcher.pas
@@ -3,7 +3,8 @@ unit UTowerArcher;
 interface
 
 uses
-  W3System, W3Components, W3Application, W3Game, W3GameApp, W3Graphics;
+  W3System, W3Components, W3Application, W3Game, W3GameApp, W3Graphics,
+  UMouseInputs;
 
 type
   TApplication = class(TW3CustomGameApplication)
@@ -22,13 +23,15 @@ implementation
 procedure TApplication.ApplicationStarting;
 begin
   inherited;
+  // Add the mouse input handlers
+  GameView.OnMouseDown := MouseDownHandler;
+  GameView.OnMouseUp := MouseUpHandler;
+  GameView.OnMouseMove := MouseMoveHandler;
 
   // Initialize refresh interval, set this to 1 for optimal speed
   GameView.Delay := 20;
 
   // Start the redraw-cycle with framecounter active
-  // Note: the framecounter impacts rendering speed. Disable
-  // the framerate for maximum speed (false)
   GameView.StartSession(True);
 end;
 
@@ -38,23 +41,16 @@ begin
   inherited;
 end;
 
-// Note: In a real live game you would try to cache as much
-// info as you can. Typical tricks are:
-//   1: Only get the width/height when resized
-//   2: Pre-calculate strings, especially RGB/RGBA values
-//   3: Only redraw what has changed, avoid a full repaint
-// The code below is just to get you started
-
 procedure TApplication.PaintView(Canvas: TW3Canvas);
 begin
   // Clear background
-  Canvas.FillStyle := 'rgb(0, 0, 99)';
+  Canvas.FillStyle := 'rgb(255, 255, 255)';
   Canvas.FillRectF(0, 0, GameView.Width, GameView.Height);
 
   // Draw our framerate on the screen
-  Canvas.Font := '10pt verdana';
-  Canvas.FillStyle := 'rgb(255, 255, 255)';
-  Canvas.FillTextF('FPS:' + IntToStr(GameView.FrameRate), 10, 20, MAX_INT);
+  // Canvas.Font := '10pt verdana';
+  // Canvas.FillStyle := 'rgb(255, 255, 255)';
+  // Canvas.FillTextF('FPS:' + IntToStr(GameView.FrameRate), 10, 20, MAX_INT);
 end;
 
 
