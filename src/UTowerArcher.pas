04df7eff1ca57110397d186c850f25b4572a763c
It423 <it42@hotmail.co.uk>
2014-11-16 11:09:06 +0000

    Apply to mouse move events

--- src/UTowerArcher.pas
+++ src/UTowerArcher.pas
@@ -3,8 +3,8 @@ unit UTowerArcher;
 interface
 
 uses
-  W3System, W3Components, W3Application, W3Game, W3GameApp, W3Graphics,
-  UMouseInputs;
+  W3System, W3Components, W3Application, W3Game, W3GameApp, W3Graphics, W3Image,
+  UMouseInputs, UArrow, UDrawing;
 
 type
   TApplication = class(TW3CustomGameApplication)
@@ -19,13 +19,17 @@ implementation
 procedure TApplication.ApplicationStarting;
 begin
   inherited;
+  // Initialize textures
+  ArrowTexture := TW3Image.Create(nil);
+  ArrowTexture.LoadFromURL("res/Arrow.png");
+
   // Add the mouse input handlers
   GameView.OnMouseDown := MouseDownHandler;
   GameView.OnMouseUp := MouseUpHandler;
   GameView.OnMouseMove := MouseMoveHandler;
 
   // Initialize refresh interval
-  GameView.Delay := 20;
+  GameView.Delay := 100;
 
   // Start the redraw-cycle with framecounter inactive
   GameView.StartSession(False);
@@ -43,6 +47,5 @@ begin
   Canvas.FillStyle := 'rgb(255, 255, 255)';
   Canvas.FillRectF(0, 0, GameView.Width, GameView.Height);
 end;
-
 
 end.
