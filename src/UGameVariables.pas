696d5274ba9edb0409dd1c91cb08965c9edb5f11
It423 <it42@hotmail.co.uk>
2014-11-17 17:50:31 +0000

    Add a FloatMod function to mod float values

--- src/UGameVariables.pas
+++ src/UGameVariables.pas
@@ -10,3 +10,15 @@ var
   PixelToPowerRatio : float;
   MaxPower : float;
   GameWidth, GameHeight : integer;
+
+// A function to perform moding operations on floats
+function FloatMod(a, b : float) : integer;
+
+implementation
+
+function FloatMod(a, b : float) : integer;
+begin
+  exit(Trunc(a - b * Trunc(a / b)));
+end;
+
+end.
