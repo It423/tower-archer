5c654e936bc8fd82143e372e6e513654183c688d
It423 <it42@hotmail.co.uk>
2014-11-17 17:11:27 +0000

    Add game variables

---
+++ src/UGameVariables.pas
@@ -0,0 +1,12 @@
+unit UGameVariables;
+
+interface
+
+uses 
+  W3System;
+
+var
+  ArrowSpawnX, ArrowSpawnY : float;
+  PixelToPowerRatio : float;
+  MaxPower : float;
+  GameWidth, GameHeight : integer;
