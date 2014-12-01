b7b4777ba8cf01e83a37be9b41d3ef9bffa383cc
It423 <it42@hotmail.co.uk>
2014-12-01 18:20:04 +0000

    Add enemy updates, drawing, collisons and add a few test enemies

--- src/UTowerArcher.pas
+++ src/UTowerArcher.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   W3System, W3Components, W3Application, W3Game, W3GameApp, W3Graphics, W3Image,
-  UMouseInputs, UArrow, UArcher, UDrawing, UGameVariables, UGameItems, UPlayerData;
+  UMouseInputs, UArrow, UArcher, UDrawing, UGameVariables, UGameItems, UPlayerData, UTextures, UGroundUnit;
 
 type
   TApplication = class(TW3CustomGameApplication)
@@ -24,6 +24,8 @@ begin
   ArrowTexture.LoadFromURL("res/Arrow.png");
   BowTexture := TW3Image.Create(nil);
   BowTexture.LoadFromURL("res/Bow.png");
+  GroundUnitTexture := TW3Image.Create(nil);
+  GroundUnitTexture.LoadFromURL("res/GroundEnemy.png");
 
   // Initialize the variables
   PixelToPowerRatio := 10;
@@ -42,6 +44,12 @@ begin
 
   // Start the redraw-cycle with framecounter inactive
   GameView.StartSession(False);
+
+
+  // Initialize a few enemies
+  Enemies[0] := TGroundUnit.Create(250, 250, 0, 5);
+  Enemies[1] := TGroundUnit.Create(300, 300, 0, 5);
+  Enemies[2] := TGroundUnit.Create(300, 500, 0, 5);
 end;
 
 procedure TApplication.ApplicationClosing;
@@ -65,12 +73,35 @@ begin
     begin
       if (Arrows[i].Active) then
         begin
+          // Get the current x and y positions for the collision engine
+          var prevX := Arrows[i].X;
+          var prevY := Arrows[i].Y;
+
+          // Move the arrow
           Arrows[i].Move();
+
+          // Check the collisions
+          Arrows[i].CheckCollisions(Enemies, prevX, prevY);
+
+          // Draw the active arrow
+          DrawArrow(Arrows[i], canvas);
         end;
     end;
 
-  // Draw game items
-  DrawArrow(Arrows, Canvas);
+  // Update enemies
+  for var i := 0 to High(Enemies) do
+    begin
+      if (Enemies[i].Health > 0) then
+        begin
+          // Move the enemy
+          Enemies[i].Move();
+
+          // Draw the enemy
+          DrawEnemy(Enemies[i], Canvas);
+        end;
+    end;
+
+  // Draw the player
   DrawArcher(Player, Canvas);
 end;
 
