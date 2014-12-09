5c25e39a788a923c6329b0d02867df520a4fe68c
It423 <it42@hotmail.co.uk>
2014-12-09 23:09:51 +0000

    Add enemy freezing capability

--- src/UEnemy.pas
+++ src/UEnemy.pas
@@ -3,15 +3,20 @@ unit UEnemy;
 interface
 
 uses 
-  W3System;
+  W3System, W3Time;
 
 type TEnemy = class(TObject)
   public
     X, Y : float;
     Health, MaxHealth : integer;
+    Frozen : boolean;
     procedure Move(); virtual; abstract;
     function GetRect() : TRectF; virtual; abstract;
     procedure Hit(damage : integer; xArrowSpeed, yArrowSpeed : float);
+    procedure Freeze(minDuration, maxDuration : integer);
+  private
+    Timer : TW3EventRepeater; // Timer for being frozen
+    function HandleTimer(sender : TObject) : boolean;
 end;
 
 implementation
@@ -24,3 +29,26 @@ begin
   // Take the damage from the health
   Health -= Round(damageWithSpeed);
 end;
+
+procedure TEnemy.Freeze(minDuration, maxDuration : integer);
+var
+  duration : integer;
+begin
+  // Get a random duration from the range
+  duration := RandomInt(maxDuration - minDuration) + minDuration;
+
+  // Tell the enemy that it is frozen
+  Frozen := true;
+
+  // Set the timer
+  Timer := TW3EventRepeater.Create(HandleTimer, duration);
+end;
+
+function TEnemy.HandleTimer(sender : TObject) : boolean;
+begin
+  Frozen := false;
+  TW3EventRepeater(sender).Free();
+  exit(true);
+end;
+
+end.
